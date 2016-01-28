8ae1242c1ca760bcda2bca7878d0935538555026
STigos <sharktigo@gmail.com>
2016-01-28 17:56:34 +0500

    Первый коммит

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Кейк плюс торты на заказ в г. Миассе </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie-edge">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="js/jquery-2.2.0.js"></script>
+  <script src="js/bootstrap.js"></script>
+</head>
+<body>
+<h1>Заголовок</h1>
+
+</body>
+</html>
